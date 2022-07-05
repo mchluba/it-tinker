72f7a26315c65f14bcc8770b5b97e1095e587534
mchluba <75523247+mchluba@users.noreply.github.com>
2022-07-05 11:36:27 +0200

    Delete ff-mar-crawler/crawler directory

--- ff-mar-crawler/crawler/EinzelEinsatz.py
+++
@@ -1,9 +0,0 @@
-class EinzelEinsatz():
-    def __init__(self, lfdnr, datetime, duration, title, incidenttype, location, ispicture):
-        self.lfdnr = lfdnr
-        self.datetime = datetime
-        self.duration = duration
-        self.title = title
-        self.incidenttype = incidenttype
-        self.location = location
-        self.ispicture = ispicture
--- ff-mar-crawler/crawler/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ["EinsatzFetcher", "EinzelEinsatz"]
-
-from .EinsatzFetcher import EinsatzFetcher
-from .EinzelEinsatz import EinzelEinsatz
